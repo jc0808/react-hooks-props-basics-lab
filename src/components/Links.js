550b140f7b5354b3fb0b5318db6f55aedfdc8a4a
Joshua Campos <jdeveloper1203@gmail.com>
2022-09-21 10:02:30 -0400

    commit tree 4b03cf3e8b13965c4c9383ed948e1bd42b828430

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Links(props) {
+    return (
+        <div>
+            <h2>Links</h2>
+            <a href={props.github}> {props.github} </a>
+            <a href={props.linkedin} > {props.linkedin}</a>
+        </div>
+    );
+}
+
+export default Links;
